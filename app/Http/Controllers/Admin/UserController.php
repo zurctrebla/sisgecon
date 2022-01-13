1116231e809ad301fedd8dacd8b9027a14a222f0
zurctrebla <albertcruz@terra.com.br>
2022-01-13 13:31:13 +0000

    first commit

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -24,7 +24,14 @@ class UserController extends Controller
     public function __construct(User $user)
     {
         $this->repository = $user;
-        $this->middleware(['can:users', 'can:users-edit', 'can:users-employee', 'can:users-profile']);
+
+        // $this->middleware(['can:users', 'can:users-edit', 'can:users-employee', 'can:users-profile']);
+
+        $this->middleware('permission:users-list|users-create|users-edit|users-delete', ['only' => ['index','show']]);
+        $this->middleware('permission:users-create', ['only' => ['create','store']]);
+        $this->middleware('permission:users-edit', ['only' => ['edit','update']]);
+        $this->middleware('permission:users-delete', ['only' => ['destroy']]);
+
     }
     /**
      * Display a listing of the resource.
