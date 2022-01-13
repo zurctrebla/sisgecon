e1b945d9d42b49761e319bbb25b85bd175b30386
zurctrebla <albertcruz@terra.com.br>
2022-01-13 13:05:36 +0000

    first commit

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -24,7 +24,7 @@ class UserController extends Controller
     public function __construct(User $user)
     {
         $this->repository = $user;
-        $this->middleware(['can:users', 'can:users-edit']);
+        $this->middleware(['can:users', 'can:users-edit', 'can:users-employee']);
     }
     /**
      * Display a listing of the resource.
