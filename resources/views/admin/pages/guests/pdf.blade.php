fc0b62d81a1c69f9c7b945df633ef2c7e11ddfd1
zurctrebla <albertcruz@terra.com.br>
2022-02-03 17:38:23 -0300

    first commit

--- resources/views/admin/pages/guests/pdf.blade.php
+++ resources/views/admin/pages/guests/pdf.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Visitante <strong>{{$guest->name}}</strong></title>
-    <link rel="stylesheet" href="{{ public('css/main.css') }}">
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/main.css') }}" />
+    {{-- <link rel="stylesheet" href="{{ public('css/main.css') }}"> --}}
 
 
 </head>
