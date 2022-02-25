a42e9f9127d1893f359a5228f3b2c31a98775c7a
Albert <albertcruz@terra.com.br>
2022-02-25 20:02:50 -0300

    first commit

--- resources/views/admin/pages/employees/pdf.blade.php
+++ resources/views/admin/pages/employees/pdf.blade.php
@@ -153,24 +153,79 @@
     </div>
     <table>
         <tr>
-          <th>Data</th>
-          <th>Entrada</th>
-          <th>Saída</th>
-          <th>Entrada</th>
-          <th>Saída</th>
+            <th style="width:120px;">Data</th>
+            <th style="width:120px; text-align:center;">Horário</th>
+            <th style="width:120px; text-align:center;">Status</th>
+            <th scope="col">Motivo</th>
         </tr>
-        @foreach ($dados as $key => $dado)
+
+@foreach ($dados as $key => $dado){{--
+<th colspan="4" style="color:rgb(0, 110, 255);">{{ date('d/m/Y',strtotime($key)) }}</th> --}}
+    <tr>
+        @for ($i = 0; $i < $dado->count(); $i++)
             <tr>
+                <td></td>
                 <td>
-                    {{ date('d/m/Y', strtotime($key)) }}
+                    {{ $dado[$i]->hour ?? '' }}
                 </td>
-                @for ($i = 0; $i < $dado->count(); $i++)
-                    <td>
-                        {{ date('H:i:s', strtotime($dado[$i]->hour)) }}
-                    </td>
-                @endfor
+                    @if ($i == 0)
+                        <td style="color:rgb(0, 167, 0);">ENTRADA</td>
+                    @endif
+                    @if ($i == 1)
+                        <td style="color:#f00;">SAÍDA</td>
+                    @endif
+                    @if ($i == 2)
+                    <td style="color:rgb(0, 167, 0);">ENTRADA</td>
+                    @endif
+                    @if ($i == 3)
+                        <td style="color:#f00;">SAÍDA</td>
+                    @endif
+                    @if ($i > 3)
+                        <td></td>
+                    @endif
+                <td>
+                    {{ $dado[$i]->reason ?? '' }}
+                </td>
             </tr>
-        @endforeach
+        @endfor
+
+            {{-- @if ( ($dado[2] ?? '') && ($dado[3] ?? ''))
+                @php
+                    $date_1t = date_diff(date_create($dado[0]->hour), date_create($dado[1]->hour))->format("%H:%I:%S") ;
+                    $date_2t = date_diff(date_create($dado[2]->hour), date_create($dado[3]->hour))->format("%H:%I:%S") ;
+
+                    $h =  strtotime($date_1t);
+                    $h2 = strtotime($date_2t);
+
+                    $minutos = date("i", $h2);
+                    $segundos = date("s", $h2);
+                    $hora = date("H", $h2);
+
+                    $temp = strtotime("+$minutos minutes", $h);
+                    $temp = strtotime("+$segundos seconds", $temp);
+                    $temp = strtotime("+$hora hours", $temp);
+
+                @endphp
+
+                <td></td>
+                <td>
+                    {{ date('H:i:s', $temp) }}
+                </td>
+                <td style="color:rgb(0,0,255);">HORAS</td>
+
+            @elseif ( ($dado[0] ?? '') && ($dado[1] ?? ''))
+
+                <td></td>
+                <td>
+                    {{ date_diff(date_create($dado[0]->hour), date_create($dado[1]->hour))->format("%H:%I:%S")  }}
+                </td>
+                <td style="color:rgb(0,0,255);">HORAS</td>
+
+            @endif --}}
+
+    </tr>
+@endforeach
+
 
     </table>
     <div hidden class="meio" style="clear:both;">
