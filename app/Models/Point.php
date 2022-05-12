c4c19ba7e63e300e78b0a6e37490fa03c47fe7b3
zurctrebla <albertcruz@terra.com.br>
2022-05-12 10:23:54 -0300

    revert last commit

--- app/Models/Point.php
+++ app/Models/Point.php
@@ -9,9 +9,9 @@ class Point extends Model
 {
     use HasFactory;
 
-    // protected $fillable = ['register', 'date', 'hour', 'reason', 'reason_status'];
+    protected $fillable = ['register', 'date', 'hour', 'reason', 'reason_status'];
 
-    protected $fillable = ['pointable_type', 'pointable_id', 'register', 'date', 'hour', 'reason', 'reason_status', 'created_at', 'updated_at'];
+    // protected $fillable = ['pointable_type', 'pointable_id', 'register', 'date', 'hour', 'reason', 'reason_status', 'created_at', 'updated_at'];
 
     public function pointable()
     {
